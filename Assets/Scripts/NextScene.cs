ee4df6af9cb3cd2e08ba23ed93d3d204ad5702f1
jacksonTowery <jackson.towery@stu.daviess.kyschools.us>
2025-01-15 11:57:57 -0600

    Title Page p1
    Program opens to a title screen where you can select the teams, then hit Start to start the battle.
    
    Still need to work on the "select team" screen

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ToBattle()
     {
-        
+        SceneManager.LoadScene("BattleScreen");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToTeam1()
     {
-        
+        SceneManager.LoadScene("Team1Selection");
+    }
+
+    public void ToTeam2()
+    {
+        SceneManager.LoadScene("Team2Selection");
     }
 }
